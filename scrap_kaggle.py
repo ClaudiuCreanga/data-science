0796dfa5945f52093f40f21d0e849ebd5d14914e
Claudiu Creanga <danutempyrium@yahoo.com>
2017-09-10 23:39:22 +0100

    kaggle statistics

--- scrap_kaggle.py
+++ scrap_kaggle.py
@@ -5,8 +5,9 @@ from w3lib.url import add_or_replace_parameter
 import re
 class MySpider(scrapy.Spider):
     name = 'myspider'
-    start_urls = ['https://www.kaggle.com/kernels/all/0?sortBy=votes&after=false']
+    start_urls = ['https://www.kaggle.com/kernels/all/20?sortBy=date&after=false']
     page = 0
+    ids = []
 
     def parse(self, response):
         data = json.loads(str(response.body, 'utf-8'))
@@ -15,13 +16,23 @@ class MySpider(scrapy.Spider):
                 "language": item["languageName"],
                 "comments": item["totalComments"],
                 "votes": item["totalVotes"],
-                "source": item["dataSources"][0]["name"],
                 "medal": item["medal"],
+                "id": item["id"],
                 "date": item["scriptVersionDateCreated"]
             }
+            id = item["id"]
             yield finalData
+
+        if id not in self.ids:
+            self.ids.append(id)
+        else:
+            logging.info("The id is duplicate, stop here")
+            return
+
         if data[len(data) - 1]["id"]:
             self.page += 20
+            if self.page > 1000:
+                self.page = 1000
             url = add_or_replace_parameter(response.url, 'after', data[len(data) - 1]["id"])
             url = re.sub(r"([0-9]){1,9}(?=\?)", str(self.page), url)
             yield scrapy.Request(url, self.parse)
