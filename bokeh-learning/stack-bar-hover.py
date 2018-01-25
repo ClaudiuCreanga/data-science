1882ab7c5f32ba5137e163d70665d4701f441635
Creanga <ccreanga@ukch-olm-014487.illumina.com>
2018-01-25 17:08:12 +0000

    new data

--- bokeh-learning/stack-bar-hover.py
+++ bokeh-learning/stack-bar-hover.py
@@ -1,35 +1,26 @@
-from bokeh.plotting import figure, output_file, show
 import numpy as np
 import pandas as pd
+import bokeh.plotting as bk
 
-from collections import OrderedDict
+np.random.seed(0)
+my_df = pd.DataFrame(
+    ["Signature 1A", "7", "11.7%", "Age"]
+    ["Signature 1B", "19", "60.7%", "Age"]
+    ["Signature 2", "16", "14.4%", "APOBEC"]
+    ["Signature 3", "3", "9.9%", "BRCA1/2 mutations"]
+    ["Signature 4", "5", "12.1%", "Smoking"]
+    ["Signature 5", "9", "14.4%"]
+    ["Signature 6", "9", "2.6%", "DNA MMR deficiency"]
+    ["Signature 7", "2", "5.0%", "Ultraviolet light (head and neck, melanoma)"]
+    ["Signature 8", "2", "2.0%", "(breast, medulloblastoma)"]
+    ["Signature 9", "2", "0.6%", "Immunoglobulin gene hypermutation (CLL, lymphoma B cell)"]
+)
 
-import pandas as pd
+p = bk.figure(
+    title = "Contribution from each signature",
+    tools="pan,box_zoom,reset,save"
+)
 
-from bokeh._legacy_charts import Bar, output_file, show
-from bokeh.sampledata.olympics2014 import data
+p.vbar(my_df, width=0.2, top=my_df[0], color=["#e31a1c", "#6a3d9a", "#f78071", "#8cd3c7", "#edf8b1", "#2877b4", "#f67f04", "#666666", "#f1b6da"])
+bk.show(p)
 
-df = pd.io.json.json_normalize(data['data'])
-
-# filter by countries with at least one medal and sort
-df = df[df['medals.total'] > 0]
-df = df.sort("medals.total", ascending=False)
-
-# get the countries and we group the data by medal type
-countries = df.abbr.values.tolist()
-gold = df['medals.gold'].astype(float).values
-silver = df['medals.silver'].astype(float).values
-bronze = df['medals.bronze'].astype(float).values
-
-# build a dict containing the grouped data
-medals = OrderedDict(bronze=bronze, silver=silver, gold=gold)
-
-# any of the following commented are also alid Bar inputs
-#medals = pd.DataFrame(medals)
-#medals = list(medals.values())
-
-output_file("stacked_bar.html")
-
-bar = Bar(medals, countries, title="Stacked bars", stacked=True)
-
-show(bar)
